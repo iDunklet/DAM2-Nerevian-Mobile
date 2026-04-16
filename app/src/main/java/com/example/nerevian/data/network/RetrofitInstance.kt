b0d9162c0647f7a5f959cddc3f002019b17c5c2f
Chenhan <cxiang2425@politecnics.barcelona>
2026-04-16 17:45:34 +0200

    update retrofitinstance

--- app/src/main/java/com/example/nerevian/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/nerevian/data/network/RetrofitInstance.kt
@@ -4,13 +4,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    // 1. Tus rutas de siempre (Tracks, etc.)
     private const val BASE_URL_MIA = "http://simex03-nereviannetapi-ectkq2-71fb90-51-83-192-177.traefik.me/"
 
-    // 2. La ruta de tu compañero (Login/Auth)
     private const val BASE_URL_AUTH = "https://nerevian.xyz/"
 
-    // Instancia para tus cosas (la que ya usas en checkTrackStatus)
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL_MIA)
@@ -19,7 +16,6 @@ object RetrofitInstance {
             .create(ApiService::class.java)
     }
 
-    // Nueva instancia para el Login
     val authApi: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL_AUTH)
